baac2f9c58350f8e37651df79baf59a06afcdda8
MichaelAuditore <michael_v613@hotmail.com>
2020-01-04 00:51:17 +0000

    [Update] The main function evaluate if there is a file opened and evaluate the instructions inside him

--- main.c
+++ main.c
@@ -1,4 +1,30 @@
 #include "monty.h"
+
+static global_t vars;
+
+/**
+ * free_vars - frees all variables into the struct global_t
+ * Return: Doesn't return
+ */
+free_vars()
+{
+	free_stack(vars.head);
+	free(vars.buffer);
+	fclose(vars.fd);
+}
+/**
+ * init - initialize all variables into the struct global_t
+ * @fd: - file descriptor of file opened
+ * Return: No return
+ */
+void init(FILE *fd)
+{
+	order = 1;
+	vars.fd = fd;
+	vars.cline = 1;
+	vars.buffer = NULL;
+	vars.stack = 1;
+}
 /**
  * main - this function evaluates the input to execute the
  * functions
@@ -9,6 +35,34 @@
 int main(int argc, char **argv)
 {
 	File *fd;
+	size_t size = 128;
+	ssize_t line_num = 0;
+	char *args[2] = {NULL, NULL};
+	void (*f)(stack_t, unsigned int);
 
-	fd = check_open(argv);
+	fd = check_open(argc, argv);
+	init(fd);
+	line_num = getline(&buffer, &size, fd);
+	while (line_num != -1)
+	{
+		args[0] = strtok(vars.buffer, " \n\t");
+		if (args[0][0] != '#' && args[0] != NULL)
+		{
+			f = get_opcode_func(args[0]);
+			if (!f)
+			{
+				dprintf(2, "L%u: ", vars.cline);
+				dprintf(2, "unknown instruction %s\n", args[0]);
+				free_vars();
+				exit(EXIT_FAILURE);
+			}
+			vars.stack = strtok(NULL, " \n\t");
+			f(&vars.head, vars.cline);
+		}
+		line_num = getline(&vars.buffer, &size, fd);
+		vars.cline++;
+	}
+
+	free_vars();
+	return (0);
 }
