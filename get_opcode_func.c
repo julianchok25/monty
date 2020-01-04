3835f0bfffcfeeb715d4e4e29df9a38e88c8058d
Miguel Angel Parada Cañon <michael_v613@hotmail.com>
2020-01-04 13:21:32 -0500

    Basic Operations (#1)
    * [ADD]monty.h: Initial header if project
    
    * [ADD]get_opcode_func: getting opcode options in a callback function and update prototype in header
    
    * New structure to keep all variables and the main function to evaluate what function will be executed in order to the instructions of file
    
    * [New file] File with all functions applied for doubly linked lists
    
    * [Correction] Syntax
    
    * [Update] The main function evaluate if there is a file opened and evaluate the instructions inside him
    
    * [Update] New prototypes added and structures
    
    * [Add] Monty program with basic operations
    
    Co-authored-by: Julian villegas <julian.villegasplus@gmail.com>

---
+++ get_opcode_func.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+/**
+ * get_opcode_func - get the option of char input to perform the opcode
+ * @s: input opcode
+ * Return: a pointer to the function given the opcode or NULL if not found
+ */
+void (*get_opcode_func(char *s))(stack_t **head, unsigned int line_num)
+{
+	int i = 0;
+	instruction_t stack[] = {
+		{"push", stack_push},
+		{"pall", stack_pall},
+		{"pint", stack_pint},
+		{"swap", stack_swap},
+		{"pop", stack_pop},
+		{"add", stack_add},
+		{"nop", stack_nop},
+		{NULL, NULL}
+	};
+
+	while (stack[i].opcode)
+	{
+		if (strcmp(stack[i].opcode, s) == 0)
+			break;
+		i += 1;
+	}
+	return (stack[i].f);
+}
---
+++ main.c
@@ -0,0 +1,43 @@
+#include "monty.h"
+global_t vars;
+/**
+ * main - this function evaluates the input to execute the
+ * functions
+ * @argc: Counter of arguments
+ * @argv: Pointer to arguments
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(int argc, char **argv)
+{
+	FILE *fd;
+	size_t size = 128;
+	ssize_t line_num = 0;
+	char *args[2] = {NULL, NULL};
+	void (*f)(stack_t **head, unsigned int line_number);
+
+	fd = check_open(argc, argv);
+	init(fd);
+	line_num = getline(&vars.buffer, &size, fd);
+	while (line_num != -1)
+	{
+		args[0] = strtok(vars.buffer, " \n\t");
+		if (args[0][0] != '#' && args[0] != NULL)
+		{
+			f = get_opcode_func(args[0]);
+			if (!f)
+			{
+				dprintf(2, "L%u: ", vars.cline);
+				dprintf(2, "unknown instruction %s\n", args[0]);
+				free_vars();
+				exit(EXIT_FAILURE);
+			}
+			vars.stack = strtok(NULL, " \n\t");
+			f(&vars.head, vars.cline);
+		}
+		line_num = getline(&vars.buffer, &size, fd);
+		vars.cline++;
+	}
+
+	free_vars();
+	return (0);
+}
