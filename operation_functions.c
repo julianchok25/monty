dd4fda5de0d22e8cb128274d3753abfb2a22eb9b
MichaelAuditore <michael_v613@hotmail.com>
2020-01-04 18:37:29 +0000

    [Fix] Betty Style

--- operation_functions.c
+++ operation_functions.c
@@ -6,23 +6,24 @@
  */
 void stack_add(stack_t **head, unsigned int line_num)
 {
-        stack_t *tmp = NULL;
-        int nel = 0;
-        tmp = *head;
-        while (tmp)
-        {
-                nel++;
-                tmp = tmp->next;
-        }
-        if (nel < 2)
-        {
-                dprintf(2, "L%u: can't add, stack too short\n", line_num);
-                free_vars();
-                exit(EXIT_FAILURE);
-        }
-        tmp = (*head)->next;
-        tmp->n += (*head)->n;
-        stack_pop(head, line_num);
+	stack_t *tmp = NULL;
+	int nel = 0;
+
+	tmp = *head;
+	while (tmp)
+	{
+		nel++;
+		tmp = tmp->next;
+	}
+	if (nel < 2)
+	{
+		dprintf(2, "L%u: can't add, stack too short\n", line_num);
+		free_vars();
+		exit(EXIT_FAILURE);
+	}
+	tmp = (*head)->next;
+	tmp->n += (*head)->n;
+	stack_pop(head, line_num);
 }
 /**
  * stack_nop - Doesn't do anything
@@ -31,6 +32,6 @@ void stack_add(stack_t **head, unsigned int line_num)
  */
 void stack_nop(stack_t **head, unsigned int line_num)
 {
-        (void)head;
-        (void)line_num;
+	(void)head;
+	(void)line_num;
 }
