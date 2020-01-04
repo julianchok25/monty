f4119cb9b5bc0f8f1ba5496fc6ba00d2bc6b5d19
Miguel Angel Parada Cañon <michael_v613@hotmail.com>
2020-01-04 15:43:03 -0500

    [Fix] Order of the stack when is LIFO or QUEUE (#5)
    * [ADD]monty.h: Initial header if project
    
    * [ADD]get_opcode_func: getting opcode options in a callback function and update prototype in header
    
    * New structure to keep all variables and the main function to evaluate what function will be executed in order to the instructions of file
    
    * [New file] File with all functions applied for doubly linked lists
    
    * [Correction] Syntax
    
    * [Update] The main function evaluate if there is a file opened and evaluate the instructions inside him
    
    * [Update] New prototypes added and structures
    
    * [Add] Monty program with basic operations
    
    * [Update] New operations for monty
    
    * [Fix] Betty Style
    
    * [Fix] Message error when try to open the file and doesn't exists
    
    * [Update] Each file represent the change to validate if a stack or queue
    
    * [Fix] Order of the stack when is LIFO or QUEUE
    
    Co-authored-by: Julian villegas <julian.villegasplus@gmail.com>

--- linked_functions.c
+++ linked_functions.c
@@ -8,7 +8,6 @@
 stack_t *add_dnodeint(stack_t **head, const int n)
 {
 	stack_t *new;
-	stack_t *tmp = *head;
 
 	if (head == NULL)
 		return (NULL);
@@ -20,14 +19,14 @@ stack_t *add_dnodeint(stack_t **head, const int n)
 		exit(EXIT_FAILURE);
 	}
 	new->n = n;
-	new->prev = NULL;
-	new->next = *head;
-
 	if (*head != NULL)
-		tmp->prev = new;
+		(*head)->prev = new;
+	new->next = *head;
+	new->prev = NULL;
 	*head = new;
 	return (*head);
 }
+
 /**
  * add_dnodeint_end - add a node at the end of the list
  * @head: pointer to the reference of the first node
@@ -36,8 +35,7 @@ stack_t *add_dnodeint(stack_t **head, const int n)
  */
 stack_t *add_dnodeint_end(stack_t **head, const int n)
 {
-	stack_t *tmp = *head;
-	stack_t *new;
+	stack_t *new, *tmp;
 
 	if (head == NULL)
 		return (NULL);
@@ -49,19 +47,22 @@ stack_t *add_dnodeint_end(stack_t **head, const int n)
 		exit(EXIT_FAILURE);
 	}
 	new->n = n;
-	new->next = NULL;
 	if (*head == NULL)
 	{
+		new->next = *head;
 		new->prev = NULL;
 		*head = new;
 		return (*head);
 	}
+	tmp = *head;
 	while (tmp->next != NULL)
 		tmp = tmp->next;
+	new->next = tmp->next;
 	new->prev = tmp;
 	tmp->next = new;
 	return (tmp->next);
 }
+
 /**
  * free_stack - free a doubly linked list
  * @head: Pointer to the first node of doubly linked list
