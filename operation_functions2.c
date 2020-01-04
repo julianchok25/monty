88daef10943bcbc1eba413ad3540b09a9d860aa2
Julian Villegas <julian.villegasplus@gmail.com>
2020-01-04 21:26:30 +0000

    [ADD]operation_functions2: Adding division function and update header prototype and opcode option

---
+++ operation_functions2.c
@@ -0,0 +1,33 @@
+#include "monty.h"
+/**
+ * stack_div - divides the second top element of stack from the top element
+ * @head: head to the stack
+ * @line_num: Reference to the line we are checking in case of an error
+ */
+void stack_div(stack_t **head, unsigned int line_num)
+{
+	stack_t *tmp = NULL;
+	int nel = 0;
+
+	tmp = *head;
+	while (tmp)
+	{
+		nel++;
+		tmp = tmp->next;
+	}
+	if (nel < 2)
+	{
+		dprintf(2, "L%u: can't div, stack too short\n", line_num);
+		free_vars();
+		exit(EXIT_FAILURE);
+	}
+	if ((*head)->n == 0)
+	{
+		dprintf(2, "L%d: division by zero\n", line_num);
+		free_vars();
+		exit(EXIT_FAILURE);
+	}
+	tmp = (*head)->next;
+	tmp->n = (int)(tmp->n) / (*head)->n;
+	stack_pop(head, line_num);
+}
